a704b3810bff899ef3fef333d289a32f1f1684ae
Saisujith9266 <2420030280@klh.edu.in>
2025-08-02 10:55:21 +0530

    Add files via upload

---
+++ control_statements/AcceptNumUntilZero.java
@@ -0,0 +1,18 @@
+package control_statements;
+import java.util.Scanner;
+public class AcceptNumUntilZero {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		while(true)
+		{
+			int n = sc.nextInt();
+			if(n==0)break;
+		}
+		System.out.print("Exiting.....");
+		sc.close();
+
+	}
+
+}
---
+++ control_statements/CheckIfPositive.java
@@ -0,0 +1,19 @@
+package control_statements;
+
+import java.util.Scanner;
+
+public class CheckIfPositive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int n = sc.nextInt();
+		if(n>0)System.out.println(n+" is positive");
+		else if(n==0) System.out.println(n+" is equal to 0");
+		else System.out.println(n+" is negetive");
+		sc.close();
+
+	}
+
+}
